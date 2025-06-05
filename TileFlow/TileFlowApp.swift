0faed36e15566beb74d30d3b193d741b4a55e6a1
Canmi <t@canmi.icu>
2025-06-05 17:24:19 +0800

    chore: init

---
+++ TileFlow/TileFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TileFlowApp.swift
+//  TileFlow
+//
+//  Created by Canmi on 2025/6/5.
+//
+
+import SwiftUI
+
+@main
+struct TileFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TileFlowTests/TileFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  TileFlowTests.swift
+//  TileFlowTests
+//
+//  Created by Canmi on 2025/6/5.
+//
+
+import Testing
+@testable import TileFlow
+
+struct TileFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
